ddd5fb9e97284aebee088004aacf83c98238dfa2
MJbae <bmj5208@gmail.com>
2020-10-23 20:50:33 +0900

    fix: 원복(MONGO_URL 숨기기 실패)

--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
 // env 활용해도 되지만 docker-compose.yml로 환경변수 관리하는게 더욱 편리
-// const dotenv = require("dotenv");
-// const env = dotenv.config({ path: ".env" });
-// if (env.error) {
-//   throw env.error;
-// }
+const dotenv = require("dotenv");
+const env = dotenv.config({ path: ".env" });
+if (env.error) {
+  throw env.error;
+}
 
 const mongoose = require("mongoose");
 const connect = mongoose
