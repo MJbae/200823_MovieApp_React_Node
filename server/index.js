a2df04c81e98f5f2b3a50c7e3ce33aa93de1054b
MJbae <bmj5208@gmail.com>
2020-10-23 23:31:22 +0900

    fix: 원복

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
+// env 활용해도 되지만 docker-compose.yml로 환경변수 관리하는게 더욱 편리
 const dotenv = require("dotenv");
 const env = dotenv.config({ path: ".env" });
 if (env.error) {
